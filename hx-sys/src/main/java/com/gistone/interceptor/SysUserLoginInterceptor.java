b3c3aff9416c4e15e80e55c5950cb4aa88148c37
wzy <229496661@qq.com>
2019-10-12 10:47:59 +0800

    no message

--- hx-sys/src/main/java/com/gistone/interceptor/SysUserLoginInterceptor.java
+++ hx-sys/src/main/java/com/gistone/interceptor/SysUserLoginInterceptor.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
-
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
