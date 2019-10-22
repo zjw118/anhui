70248c2707e2c1475712727c338452dad2b605ab
zhoufei <zf1017@foxmail.com>
2019-10-22 19:18:42 +0800

    调试bug

--- NatureResUtilManager/src/main/java/com/gistone/bjhky/util/ConfigUtil.java
+++ NatureResUtilManager/src/main/java/com/gistone/bjhky/util/ConfigUtil.java
@@ -1,5 +1,6 @@
 package com.gistone.bjhky.util;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
