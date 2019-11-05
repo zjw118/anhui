0ade66c0ce47a420146c09704c5ffd928469b4a9
wzy <229496661@qq.com>
2019-11-05 10:57:33 +0800

    no message

--- hx-sys/src/main/java/com/gistone/mapper/LoginLogMapper.java
+++ hx-sys/src/main/java/com/gistone/mapper/LoginLogMapper.java
@@ -2,6 +2,9 @@ package com.gistone.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.gistone.entity.LoginLog;
+import com.gistone.util.PageBean;
+
+import java.util.List;
 
 /**
  * <p>
@@ -12,5 +15,11 @@ import com.gistone.entity.LoginLog;
  * @since 2019-04-03
  */
 public interface LoginLogMapper extends BaseMapper<LoginLog> {
+    // 	分页查询-获取总条量
+    int getPoSum(PageBean pageBean);
+    //	分页查询-获取数据
+    List<Object> selectPoList(PageBean pageBean);
+
+
 
 }
