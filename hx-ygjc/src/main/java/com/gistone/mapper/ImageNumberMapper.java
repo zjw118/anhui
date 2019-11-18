c751c67c5d7e5c0b183a79235b4a76bcdca686fc
wzy <229496661@qq.com>
2019-11-18 19:08:14 +0800

    no message

--- hx-ygjc/src/main/java/com/gistone/mapper/ImageNumberMapper.java
+++ hx-ygjc/src/main/java/com/gistone/mapper/ImageNumberMapper.java
@@ -1,7 +1,5 @@
 package com.gistone.mapper;
 
-import com.gistone.entity.Image;
-import com.gistone.entity.ImageConfig;
 import com.gistone.entity.ImageNumber;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
@@ -11,19 +9,21 @@ import java.util.List;
 @Mapper
 public interface ImageNumberMapper{
 
-    //影像外键查询列表
-    List<ImageNumber> selectImageNumberByImageId(@Param(value="id") Integer id);
-
-    //影像外键查询类型系数列表
-    List<ImageConfig> selectImageConfigByImageId(@Param(value="id") Integer id);
-
     //添加
     int insertImageNumber(ImageNumber imageNumber);
 
-    //影像外键真删
-    int deleteImageNumberByImageId(Image image);
+    //主键真删
+    int deleteImageNumber(@Param(value="id")Integer id);
 
-    //类型外键删除
-//    int deleteByImageConfigId(@Param(value="id") Integer id);
+    //获取所有批次名
+    List<String> selectName();
+
+    //批次名查询列表
+    List<ImageNumber> selectImageNumber(@Param(value="name")String name);
+
+    //修改
+    int updateImageNumber(ImageNumber imageNumber);
+
+
 
 }
