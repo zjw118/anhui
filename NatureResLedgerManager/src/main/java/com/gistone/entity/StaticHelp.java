9cc8081e4975e668783ebec22651696e63bec54d
zhaojingwei <zhaojingwei@gistone.cn>
2019-12-11 15:26:16 +0800

    质量评估导出数量不一致问题

--- NatureResLedgerManager/src/main/java/com/gistone/entity/StaticHelp.java
+++ NatureResLedgerManager/src/main/java/com/gistone/entity/StaticHelp.java
@@ -1,14 +1,32 @@
 package com.gistone.entity;
 
+import cn.afterturn.easypoi.excel.annotation.Excel;
+import cn.afterturn.easypoi.excel.annotation.ExcelIgnore;
 import lombok.Data;
 
 @Data
 public class StaticHelp {
+
+
+    @ExcelIgnore
     private String cd001;
 
+
+    /**
+     * 原人类活动名称
+     */
+    @Excel(name = "原人类活动名称",height = 11, width = 15)
     private String name;
+    /**
+     * 原斑块数量
+     */
+    @Excel(name = "原人类活动斑块数量",height = 11, width = 15)
+    private String orignCount="0";
 
-    private String orignCount;
+    /**
+     * 现在斑块活动数目
+     */
+    @Excel(name = "核查后人类活动斑块数量",height = 11, width = 25)
+    private String nowsCount="0";
 
-    private String nowsCount;
 }
